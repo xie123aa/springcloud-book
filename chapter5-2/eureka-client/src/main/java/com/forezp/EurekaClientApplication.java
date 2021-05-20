daec52249a7d7902881161d54076c55e5a8199a3
jiazhi.xie 谢佳智 <Jiazhi_xie@wishbuild.com>
2021-05-20 15:21:08 +0800

    eureka学习

--- chapter5-2/eureka-client/src/main/java/com/forezp/EurekaClientApplication.java
+++ chapter5-2/eureka-client/src/main/java/com/forezp/EurekaClientApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+//客户端可以被发现
 @EnableEurekaClient
 public class EurekaClientApplication {
 
--- chapter5-2/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ chapter5-2/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -3,7 +3,7 @@ package com.forezp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//开启服务端功能
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
