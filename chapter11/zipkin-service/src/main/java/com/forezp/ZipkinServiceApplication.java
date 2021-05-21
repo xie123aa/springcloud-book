67f16b87441391066da8266d5938ae12115778b4
jiazhi.xie 谢佳智 <Jiazhi_xie@wishbuild.com>
2021-05-21 14:55:35 +0800

    ZipkinServer服务搭建

--- chapter11/zipkin-service/src/main/java/com/forezp/ZipkinServiceApplication.java
+++ chapter11/zipkin-service/src/main/java/com/forezp/ZipkinServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+//3.开启功能
 @EnableZipkinServer
 public class ZipkinServiceApplication {
 
